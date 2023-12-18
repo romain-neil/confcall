dff8a9890249164f8e7934a7dbe3a87547416720
Romain Neil <contact@romain-neil.fr>
2023-12-18 20:01:48 +0100

    refactor: remove unused interface

--- src/Security/CustomLdapUserProvider.php
+++ src/Security/CustomLdapUserProvider.php
@@ -5,7 +5,6 @@ use InvalidArgumentException;
 use Symfony\Component\Ldap\Entry;
 use Symfony\Component\Ldap\Security\LdapUser;
 use Symfony\Component\Ldap\Security\LdapUserProvider;
-use Symfony\Component\Security\Core\User\UserInterface;
 use function count;
 
 class CustomLdapUserProvider extends LdapUserProvider {
